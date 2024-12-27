f2a416d3f53952265740e15a8c470fe5cbe60799
Clément Noël <noyelclement@hotmail.fr>
2024-12-27 11:30:57 +0100

    feat: handle player_animation and reorder Player nodes

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,9 @@ class_name Player
 extends CharacterBody2D
 
 var speed := 100
+var looking_left := true
+
+@onready var sprite = $Sprite2D
 
 func _process(_delta: float) -> void:
 	# On va gérer les mouvements dans la fonction _process plutôt que _input
@@ -9,6 +12,16 @@ func _process(_delta: float) -> void:
 	# ne lit le mouvement QUE quand la touche est appuyé / relâché, et non pas continuellement 
 	# (contrairement à _process qui s'exécute toute les frames)
 	player_movement()
+	player_animation()
+
+func player_animation() -> void:
+	print(velocity.x)
+	if velocity.x > 0 and looking_left:
+		sprite.flip_h = true
+		looking_left = false
+	elif velocity.x < 0 and !looking_left:
+		sprite.flip_h = false
+		looking_left = true
 
 func player_movement() -> void:
 	var input := Input.get_vector("Left", "Right", "Up", "Down")
